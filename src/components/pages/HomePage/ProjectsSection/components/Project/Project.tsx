36dc64f2182087e39af15197e9b0f8075fdc05e2
terminaate <tntwnik@gmail.com>
2023-03-06 12:35:42 +0300

    Change text stroke animation on project hover

--- src/components/pages/HomePage/ProjectsSection/components/Project/Project.tsx
+++ src/components/pages/HomePage/ProjectsSection/components/Project/Project.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react';
 import cl from './Project.module.scss';
+import Image from 'next/image';
 
 export interface IProject {
   type: string;
@@ -15,7 +16,7 @@ const Project: FC<IProject> = ({ image, type, title, description }) => {
         <span className={cl.projectType}>{type}</span>
         <h2 className={cl.projectTitle}>{title}</h2>
       </div>
-      <img src={image} alt={title} className={cl.projectImage} />
+      <Image src={image} alt={title} className={cl.projectImage} />
       <pre className={cl.projectDescription}>{description}</pre>
     </div>
   );
